62ecae651d2b6ae97511f3ed6bd2dabcd86a3b35
Mahima Singh <Mahima Singh>
2017-04-16 22:40:24 -0400

    Nothing

--- df.py
+++ df.py
@@ -1,5 +1,10 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import dh
+import os
+import sys
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
 parameters = dh.generate_parameters(generator=2, key_size=512,
                                      backend=default_backend())
 
@@ -11,3 +16,52 @@ bob_public_key = bob_b.public_key() ## This is g^b MOD p
 
 alice_shared_key = alice_a.exchange(bob_public_key)
 bob_shared_key = bob_b.exchange(alice_public_key)
+
+
+
+aes_key = alice_shared_key[0:32]
+
+
+
+
+iv = os.urandom(12)
+f = open("f1.txt", 'rb')
+output = open("f2.txt", 'wb')
+decrypted = open("f3.txt",'wb')
+plaintext = f.read()
+
+    # Construct an AES-GCM Cipher object with the given key and a
+    # randomly generated IV.
+encryptor = Cipher(
+		 algorithms.AES(aes_key),
+        modes.GCM(iv),
+        backend=default_backend()
+).encryptor()
+
+    # associated_data will be authenticated but not encrypted,
+    # it must also be passed in on decryption.
+    
+
+    # Encrypt the plaintext and get the associated ciphertext.
+    # GCM does not require padding.
+ciphertext = encryptor.update(plaintext) + encryptor.finalize()
+
+    
+
+output.write(ciphertext)
+
+tag = encryptor.tag
+
+    
+decryptor = Cipher(
+         algorithms.AES(aes_key),
+         modes.GCM(iv, tag),
+         backend=default_backend()
+).decryptor()
+
+text =  decryptor.update(ciphertext) + decryptor.finalize()
+decrypted.write(text)
+
+f.close()
+output.close()
+decrypted.close()
